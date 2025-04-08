a7f9d4f61939cb49e1fa8bbb3bba66e3334b513d
MsOogway <nikolay.j4job@yandex.ru>
2025-04-08 16:52:17 +0300

    init

---
+++ src/main/java/com/r_style/softlub/myCV/MyCvApplication.java
@@ -0,0 +1,13 @@
+package com.r_style.softlub.myCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r_style/softlub/myCV/MyCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r_style.softlub.myCV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
